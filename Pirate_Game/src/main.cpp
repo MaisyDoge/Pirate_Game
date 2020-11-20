b2b808b4b392ef4132f66dbd1c48e236e81bd064
SpencerCodes <33442723+SpencerCodes@users.noreply.github.com>
2020-11-19 21:37:19 -0800

    Notes

--- Pirate_Game/src/main.cpp
+++ Pirate_Game/src/main.cpp
@@ -2,8 +2,18 @@
 using namespace std;
 
 int main() {
-	cout << "Hello World\n";
-	cout << "Do you see this?\n";
-	cout << "Yes, I in fact see this.\n";
+	/*
+	General
+	Pirate Game, Arcade Based with high scores
+	Progress through Europe, assuming your men don't kill you on the way and you don't get conquered.
+	Start in crete, go to mainland greece, sicily, western side of italy, rome, france, spain, portugal, then atlantic, finishing in america.
+	As time goes on have newer weapons avalable for purchase
+	Each town has a random amount of wealth based on difficulty and location
+	
+	Specific
+	Pay your men a salary 
+	Hire crew with differnt skill sets, captain assistant, crew hands,
+	*/
+	cout << "Testing Running the code";
 	return 0;
 }
